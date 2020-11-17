4df6e220f488a182d05aa350ce54dffb9df107a1
Logan Jones <loganemilyjones@mac.com>
2020-11-16 18:12:19 -0800

    Basic server setup

---
+++ server.js
@@ -0,0 +1,152 @@
+'use strict';
+
+// Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+
+// Environment variables
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+// Setup application
+const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', homeHandler);
+
+// Route Handlers
+function homeHandler(req, res) {
+  res.status(200).render('index.ejs');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------- Board Game Stuff --------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------- Trivia Stuff --------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------- Playlist Stuff --------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start the server
+app.listen(PORT, () => console.log(`Server now listening on port ${PORT}.`));
