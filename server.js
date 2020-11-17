db3cfbddfd723e43c3e2c873352857245acb2689
Michael Mandell <michael@massagepractice101.com>
2020-11-16 21:48:03 -0800

    safe place holding pattern

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const pg = require('pg');
 
 // Environment variables
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2021;
 
 // Setup application
 const app = express();
 const client = new pg.Client(process.env.DATABASE_URL);
+
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
@@ -50,8 +51,25 @@ function homeHandler(req, res) {
 
 // -------- Board Game Stuff --------------//
 
+/*
+https://api.boardgameatlas.com/api/search?order_by=popularity&ascending=false&client_id=JLBr5npPhV
+https://www.boardgameatlas.com/search/
 
 
+*/
+app.get('/boardgames', bgamesSearch);
+
+function bgamesSearch (req, res) {
+  console.log('Function Commit');
+  console.log('Response = ', res);
+  const clientID = process.env.CLIENT_ID;
+  const title = ('title = ', res.body.gamename);
+  console.log('This Search=', title);
+  // const bgamesURL = `https://api.boardgameatlas.com/api/search?name=${ title }&client_id=${ clientID }`;
+  const bgamesURL = `https://api.boardgameatlas.com/api/search?name=Pirates&client_id=${ clientID }`;
+  console.log('Search Games URL: ', bgamesURL);
+  res.status(200).render('pages/boardgames');
+}
 
 
 
