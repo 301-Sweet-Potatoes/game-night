790a3a4ff7184bc39afeda3bf25acf0ae35353fe
Michael Mandell <michael@massagepractice101.com>
2020-11-21 14:25:05 -0800

    added some css to fav page

--- data/trivia.sql
+++ data/trivia.sql
@@ -5,4 +5,43 @@ CREATE TABLE trivia (
   category VARCHAR(500),
   question VARCHAR(500),
   correctanswer VARCHAR(500)
-)
+);
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
+
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
+
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
+
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
+
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
+
+INSERT INTO 
+trivia(category, question, correctanswer)
+VALUES 
+('Science: Computers', 'All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.', 'False');
